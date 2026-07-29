d88ae7d90393eb0fa314e43e4f70da6d33ce122d
321Modular <projetos.madeireira.pacheco@gmail.com>
2026-07-29 11:28:32 -0300

    Add files via upload

--- shared/runtime-config.js
+++ shared/runtime-config.js
@@ -1,5 +1,5 @@
 window.SUPERAPP_CONFIG = {
-  "environment": "homologacao-cloud",
+  "environment": "homologacao",
   "supabaseUrl": "https://hrpeivwoppwzrchypmhy.supabase.co",
   "supabasePublishableKey": "sb_publishable_w6zuDGQzVTLUeTSv4Q8SNQ_ke9BHV1S",
   "authSchema": "core",
